26601bf9a990644d8607fe71bbdc0a8e30bf55d1
Umberto Valente <uvalente89@gmail.com>
2020-07-30 09:16:13 +0100

    :wrench: add small screen index support

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ const Index = () => (
     <div className='relative w-full h-screen bg-gray-800 font-mono'>
       <img src='/medium-test.jpg' className='relative w-full h-full object-cover object-top opacity-75 z-0' />
       <div className='absolute top-0 z-10 w-full h-full flex justify-center items-center text-gray-100 flex-col divide-y-2 divide-opacity-50 divide-blue-500'>
-        <h1 className='text-6xl font-semibold'>
+        <h1 className='font-semibold text-4xl md:text-6xl'>
           Umberto Valente
         </h1>
-        <h2 className='text-3xl italic pt-1'>
+        <h2 className='italic text-xl md:text-3xl md:pt-1'>
           Full Stack Software Developer
         </h2>
       </div>
