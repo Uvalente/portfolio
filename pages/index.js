de4eef51389dede97ec1dc0868da90ab3eb76c94
Umberto Valente <uvalente89@gmail.com>
2020-07-30 03:32:18 +0100

    :lipstick: add initial nav styling

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Layout from '../components/Layout'
 
 const Index = () => (
   <Layout pageTitle='uValente Index'>
-    <p className='text-green-700 text-4xl'>
+    <p className='pt-16 text-green-700 text-4xl'>
       Hello world!
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </p>
   </Layout>
 )
