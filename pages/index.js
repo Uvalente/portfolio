d5ba898a1ac918a1f1524027ce8f5ffc94909c1e
Umberto Valente <uvalente89@gmail.com>
2020-07-30 06:33:10 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import Layout from '../components/Layout'
+import { useEffect, useState } from 'react'
+// IMPORT useEffect and useState
+// check Prerendered page badge
+// to see if anything change
 
-const Index = () => (
-  <Layout pageTitle='uValente Index'>
-    <p className='pt-16 text-green-700 text-4xl'>
-      Hello world!
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-    </p>
-  </Layout>
-)
+const Index = () => {
+  const [test, setTest] = useState('false')
+
+  const change = () => {
+    setTest('true')
+  }
+  useEffect(() => {
+    console.log('HI')
+  }, [])
+
+  return (
+    <Layout pageTitle='uValente Index'>
+      <p className='pt-16 text-green-700 text-4xl'>
+        Hello world!
+        <button onClick={change}>CLICK</button>
+        state: {test}
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      </p>
+    </Layout>
+  )
+}
 
 export default Index
