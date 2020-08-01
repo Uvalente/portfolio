3ddd13bd5a7da3645b3a5e5b1e60acb30c850bfa
Umberto Valente <uvalente89@gmail.com>
2020-08-01 18:31:07 +0100

    :sparkles: add projects index fetching

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,19 +1,54 @@
 import Layout from '../../components/Layout'
 import Project from '../../components/Project'
+import matter from 'gray-matter'
+import Link from 'next/link'
 
 const description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
 
+const Projects = ({ projects, ...props }) => {
+  const projectsList = projects.map((project) => {
+    console.log(project)
+    return (
+      <Link href='/projects/[projectName]' as={`/projects/${project.slug}`} key={project.slug} >
+        <a>
+          <Project title={project.data.title} description={project.data.description} stack='ruby javascript' imgSrc='/stock-mock.PNG' />
+        </a>
+      </Link>
+    )
+  })
 
-const Projects = () => (
-  <Layout pageTitle='uValente Projects'>
-    <div className='bg-dark-blue font-mono text-gray-100 pb-20'>
-      hi
-      <br /><br /><br /><br /><br /><br /><br />
 
-      <Project title='Test project' description={description} stack='ruby javascript' imgSrc='/stock-mock.PNG' />
-    </div>
+  return (
+    <Layout pageTitle='uValente Projects'>
+      <div className='bg-dark-blue font-mono text-gray-100 pb-20'>
+        {projectsList}
+      </div>
+    </Layout>
+  )
+}
 
-  </Layout>
-)
+export async function getStaticProps() {
+  const projects = ((context) => {
+    const keys = context.keys()
+    const values = keys.map(context)
+
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
+      const value = values[index]
+      const document = matter(value.default)
+      return {
+        data: document.data,
+        slug,
+      }
+    })
+    return data
+  })(require.context('../../projects', true, /\.md$/))
+
+  return {
+    props: {
+      projects
+    },
+  }
+}
 
 export default Projects
