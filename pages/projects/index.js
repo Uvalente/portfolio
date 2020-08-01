c723e87f1080b6115618a09042633301a70fc2c1
Umberto Valente <uvalente89@gmail.com>
2020-08-01 17:02:01 +0100

    :sparkles: add Project component

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,18 @@
 import Layout from '../../components/Layout'
+import Project from '../../components/Project'
+
+const description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+
 
 const Projects = () => (
   <Layout pageTitle='uValente Projects'>
-    Hello
+    <div className='bg-dark-blue font-mono text-gray-100 pb-20'>
+      hi
+      <br /><br /><br /><br /><br /><br /><br />
+
+      <Project title='Test project' description={description} stack='ruby javascript' imgSrc='/stock-mock.PNG' />
+    </div>
+
   </Layout>
 )
 
