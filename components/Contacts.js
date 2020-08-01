0e809b802f8a9df154b554ac7693ef35102da25a
Umberto Valente <uvalente89@gmail.com>
2020-08-01 12:02:21 +0100

    :art: change form button

--- components/Contacts.js
+++ components/Contacts.js
@@ -20,7 +20,10 @@ h
         <textarea name="message" id='message' className='py-2 px-2 text-gray-900 w-full focus:outline-none focus:shadow-outline rounded'/>
       </div>
 
-      <button type="submit" className='mt-8 rounded text-gray-100 px-4 py-1 bg-dark-blue hover:shadow-inner hover:bg-blue-800 transition-all duration-300 uppercase text-sm tracking-wider'>Send</button>
+      
+      <button type="submit" className='mt-8 rounded-full text-gray-100 w-10 h-10 p-2 bg-dark-blue hover:shadow-inner hover:bg-blue-800 transition-all duration-300 uppercase text-sm tracking-wider'>
+      <svg fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+      </button>
     </form>
   </div>
 )
