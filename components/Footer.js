14133e354f2c937b6365272d25e38546a9ca8b87
Umberto Valente <uvalente89@gmail.com>
2020-08-02 19:10:55 +0100

    :art: add oblique div to [project]

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
   <>
-    <div className='pt-16 h- bg-dark-blue skew-y-3 text-gray-100 transform flex flex-col justify-center' >
+    <div className='pt-16 bg-dark-blue skew-y-3 text-gray-100 transform flex flex-col justify-center' >
       <a href='#top' className='self-center -skew-y-3 transform'>
         <svg className='h-8 animate-bounce' fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd"></path></svg>
       </a>
