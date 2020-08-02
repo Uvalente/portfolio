12eaf8a0a3260863a985fc453b0449ca45be720f
Umberto Valente <uvalente89@gmail.com>
2020-08-02 19:16:53 +0100

    :art: add shadow to Project

--- components/Project.js
+++ components/Project.js
@@ -1,5 +1,5 @@
 const Project = ({ imgSrc, title, description, stack, ...props }) => (
-  <div className="max-w-sm w-full md:max-w-xs lg:max-w-lg lg:flex lg:h-full">
+  <div className="max-w-sm w-full md:max-w-xs lg:max-w-lg lg:flex lg:h-full shadow-lg">
     <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: `url(${imgSrc})` }} title={title} >
     </div>
     <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
