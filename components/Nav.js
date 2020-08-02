9abecb31b6e11e3f3eadd2baf930306846a3e6e9
Umberto Valente <uvalente89@gmail.com>
2020-08-02 12:35:15 +0100

    :sparkles: add lg screen nav

--- components/Nav.js
+++ components/Nav.js
@@ -9,14 +9,14 @@ const Nav = () => {
   }
 
   return (
-    <nav className='fixed w-full z-50 bg-dark-blue text-gray-100 bg-opacity-75'>
-      <div className='w-full flex justify-between items-center px-4 md:px-16 '>
+    <nav className='fixed w-full z-50 bg-dark-blue text-gray-100 bg-opacity-75 md:flex md:items-center md:justify-between'>
+      <div className='w-full flex justify-between items-center px-4 md:pl-16 md:pr-0 md:w-auto '>
         <Link href='/'>
           <a>
             <p className='font-mono text-5xl'>uValente</p>
           </a>
         </Link>
-        <button className='w-10 h-auto focus:outline-none' onClick={handleClick}>
+        <button className='w-10 h-auto focus:outline-none md:hidden' onClick={handleClick}>
           <svg fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor" >
             {
               isClose ?
@@ -27,15 +27,15 @@ const Nav = () => {
           </svg>
         </button>
       </div>
-      <div className={`${isClose && 'hidden'} px-2 pt-3 pb-4 font-semibold tracking-wider text-lg flex flex-col`}>
+      <div className={`${isClose && 'hidden'} px-2 pt-3 pb-4 font-semibold tracking-wider text-lg flex flex-col md:block md:pr-16 md:space-x-3`}>
         <Link href='/'>
-          <a className='hover:shadow-inner hover:bg-blue-800 transition-all duration-300 rounded px-2 py-1'>Home</a>
+          <a className='hover:shadow-inner hover:bg-blue-800 transition-all duration-300 rounded px-2 py-1 md:px-4'>Home</a>
         </Link>
         <Link href='/projects'>
-          <a className='hover:shadow-inner hover:bg-blue-800 transition-all duration-300 rounded px-2 py-1 mt-1'>Projects</a>
+          <a className='hover:shadow-inner hover:bg-blue-800 transition-all duration-300 rounded px-2 py-1 mt-1 md:px-4'>Projects</a>
         </Link>
         <Link href='/#contacts'>
-          <a className='hover:shadow-inner hover:bg-blue-800 transition-all duration-300 rounded px-2 py-1 mt-1'>Contacts</a>
+          <a className='hover:shadow-inner hover:bg-blue-800 transition-all duration-300 rounded px-2 py-1 mt-1 md:px-4'>Contacts</a>
         </Link>
       </div>
     </nav>
