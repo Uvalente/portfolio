87dc50e972fddbd21e3f54ff41b20fcddba3e8c0
Umberto Valente <uvalente89@gmail.com>
2020-08-03 06:34:32 +0100

    :wrench: add about section

--- components/Main.js
+++ components/Main.js
@@ -1,14 +1,32 @@
-const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur mollis dui. Duis lobortis, justo sed gravida faucibus, risus enim hendrerit ante, mollis egestas eros leo sit amet nibh. Nullam interdum euismod nibh, in mattis risus suscipit quis. Praesent feugiat metus sit amet quam laoreet faucibus. Sed faucibus, metus a lobortis iaculis, ligula erat egestas nulla, et dictum arcu augue tristique neque. Suspendisse porta, ante sed congue scelerisque, ipsum ante vehicula nulla, nec porttitor neque ipsum eget risus. Fusce ullamcorper rhoncus arcu non tristique. Praesent tempor tempus convallis Curabitur pretium odio eget purus cursus sodales. Mauris egestas nisl arcu, eget porttitor tellus rutrum ac. Morbi eu posuere ligula. Nullam varius vehicula turpis sit amet blandit. Pellentesque condimentum purus vel dolor fringilla, non suscipit leo feugiat. Pellentesque nibh ex, commodo id magna sit amet, facilisis condimentum arcu. Fusce quis vestibulum turpis. Aliquam eu magna sed nisi dictum facilisis<br>\n Duis vitae velit imperdiet, porttitor ipsum id, volutpat nisl. Cras convallis rhoncus massa ut accumsan. Etiam congue nulla vel libero volutpat pharetra. Nulla sodales, massa sed sagittis congue, orci leo ullamcorper mauris, ac cursus velit augue in ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer augue lectus, feugiat sed ultrices id, molestie at magna. Morbi et malesuada libero."
-
 const Main = () => (
   <>
     <div className='relative z-10 flex flex-col items-center lg:flex-row justify-center bg-dark-blue text-gray-100 pt-32 font-mono' id='top'>
       <div className='lg:w-1/2 xl:w-1/3'>
         <img className='h-64 w-64 rounded-full object-cover object-top overflow-hidden lg:h-auto lg:w-full lg:rounded-none' src='/umberto.jpg' />
       </div>
-      <div className='pt-16 px-8 lg:w-1/2 xl:w-1/3'>
-        <h3>TITLE</h3>
-        {lorem}
+      <div className='pt-16 px-8 lg:w-1/2 xl:w-1/3 space-y-6 text-lg md:space-y-4 tracking-wide'>
+        <p>After many years surrounded by hobs and pots, I've hung up my knives and dust off my childhood dream.</p>
+        <p>
+          Equipped with the same drive and perseverance that allowed me to rise and shine in the culinary world, I embarked on countless hours of self-study and self-improvement.
+        </p>
+        <p>
+          I honed my skills in a well-known coding boot-camp, absorbed the agile mentality, and thrived in pair programming.
+        </p>
+        <p>
+          I admit it wasn't always an easy road, however, the feeling of developing an idea makes up for it.
+        </p>
+        <p>
+          I found that coding is more than solving a problem or following instructions, it is also about being creative and look out for new perspectives.
+        </p>
+        <p>
+          During these months, I had the opportunity to work on a few personal and collaborative projects and I am now looking for new exciting and challenging proposals that will boost my expertise and technical skills.
+        </p>
+        <p>
+          If you would like to learn more about myself and my experience, feel free to reach out or have a look at my newest projects.
+        </p>
+        <p className='italic text-right pt-16 tracking-normal'>
+          What once was art on a plate can now be art on a screen!
+        </p>
       </div>
     </div>
     <div className='relative z-0 -mt-32 h-64 bg-dark-blue -skew-y-6 transform xxl:-skew-y-3' />
