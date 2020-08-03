aef7e9ade5ece79c45a56b733628c557c4888220
Umberto Valente <uvalente89@gmail.com>
2020-08-03 07:15:16 +0100

    :hammer: restore pt on smaller screens, add some spacing to about text

--- components/Main.js
+++ components/Main.js
@@ -4,12 +4,12 @@ const Main = () => (
       <div className='lg:w-1/3 xl:w-28p xxl:w-1/5'>
         <img className='h-64 w-64 rounded-full object-cover object-top overflow-hidden lg:h-auto lg:w-full lg:rounded-none' src='/umberto.jpg' />
       </div>
-      <div className='px-8 lg:w-1/2 xxl:w-1/3 space-y-6 text-lg md:space-y-4 tracking-wide'>
+      <div className='pt-16 lg:pt-0 px-8 lg:w-1/2 xxl:w-1/3 space-y-6 text-lg md:space-y-4 tracking-wide'>
         <p>After many years surrounded by hobs and pots, I've hung up my knives and dust off my childhood dream.</p>
         <p>
           Equipped with the same drive and perseverance that allowed me to rise and shine in the culinary world, I embarked on countless hours of self-study and self-improvement.
         </p>
-        <p>
+        <p className='pt-6'>
           I honed my skills in a well-known coding boot-camp, absorbed the agile mentality, and thrived in pair programming.
         </p>
         <p>
@@ -18,7 +18,7 @@ const Main = () => (
         <p>
           I found that coding is more than solving a problem or following instructions, it is also about being creative and look out for new perspectives.
         </p>
-        <p>
+        <p className='pt-6'>
           During these months, I had the opportunity to work on a few personal and collaborative projects and I am now looking for new exciting and challenging proposals that will boost my expertise and technical skills.
         </p>
         <p>
