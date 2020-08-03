827589e47b667a97a5e10fe675e3f6203cd171aa
Umberto Valente <uvalente89@gmail.com>
2020-08-03 08:31:12 +0100

    :hammer: update HeroImage bottom text

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -11,7 +11,7 @@ const HeroImage = ({ imgSrc, ...props }) => (
     </div>
     <div className='absolute w-full bottom-0 pb-6 flex flex-col text-center z-10'>
       <p className='text-xl'>
-        Learn more about what I do
+        Learn more about me
       </p>
       <a className='self-center' href='#top'>
         <svg className='mt-2 h-8 animate-bounce' fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
